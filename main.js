7f5ca4cbe285b1ab9ca1626fdbd7225ac507f4ac
jonhtan <jmtrs@rede.ulbra.br>
2020-02-13 21:06:13 -0300

    inicial do localstorage

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 class Tarefa{
-    constructor()
+    constructor() {}
 }
+
+
+
+
+
+
+class LStorage{
+    constructor(data, titulo, status) {
+        this.data = data;
+        this.titulo = titulo;
+        this.status = status;
+        window.localStorage.setItem("teste", "aaa");
+        window.localStorage.setItem("teste2", "aaa2");
+    }
+
+    salvar(){
+        window.localStorage.setItem(this.titulo, {"data": this.data, "titulo": this.titulo, "status": this.status});
+        console.log("task salva localmente.");
+    }
+
+    //retorna uma array com todas as keys armazenadas para serem acessadas
+    getKeys() {
+        this.keys = [];
+        for(let b in Object.entries(localStorage)){
+            //pegando as chaves e salvando na lista.
+            this.keys.push(window.localStorage.getItem(Object.entries(localStorage)[b][0]))
+        };
+    }
+}
+
+global_tasks = new LStorage()
+console.log("keys salvas no storage: " + global_tasks.getKeys())
