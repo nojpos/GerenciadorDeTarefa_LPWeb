4769bfbc4966a26d61472a5d3e1c6786668c82f7
Jonathas Neto Ribeiro <joneto@rede.ulbra.br>
2020-02-13 21:25:11 -0300

    Criando classe tarefa

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-class Tarefa{
-    constructor()
+class Tarefa {
+    constructor(data, titulo, status) {
+        this.date = data;
+        this.title = titulo;
+        this.status = status;
+
+    }
+
+    
+    adicionarTarefa(data, titulo) {
+
+        t1 = new Tarefa('12/02/2020', 'lavar o carro', 'false')
+        console.log(t1)
+    }
 }
