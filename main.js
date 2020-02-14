a667c479429b5a79b28081da9ed1ef917ff9ae6c
jonhtan <jmtrs@rede.ulbra.br>
2020-02-13 21:20:08 -0300

    interação com o localstorage

--- main.js
+++ main.js
@@ -12,24 +12,32 @@ class LStorage{
         this.data = data;
         this.titulo = titulo;
         this.status = status;
+        //testes
         window.localStorage.setItem("teste", "aaa");
         window.localStorage.setItem("teste2", "aaa2");
     }
 
+    //salvar no localStorage.
     salvar(){
         window.localStorage.setItem(this.titulo, {"data": this.data, "titulo": this.titulo, "status": this.status});
         console.log("task salva localmente.");
     }
 
-    //retorna uma array com todas as keys armazenadas para serem acessadas
+    //retorna a tarefa salva localmente.
+    getTask(chave){
+        return window.localStorage.getItem(chave);
+    }
+
+    //retorna uma array com todas as keys armazenadas para serem acessadas.
     getKeys() {
         this.keys = [];
         for(let b in Object.entries(localStorage)){
             //pegando as chaves e salvando na lista.
-            this.keys.push(window.localStorage.getItem(Object.entries(localStorage)[b][0]))
+            this.keys.push(window.localStorage.key(b));
         };
+        return this.keys;
     }
 }
 
-global_tasks = new LStorage()
-console.log("keys salvas no storage: " + global_tasks.getKeys())
+g_tasks = new LStorage();
+console.log("keys salvas no storage: " + g_tasks.getKeys());
