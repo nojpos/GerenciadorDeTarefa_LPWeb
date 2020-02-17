1230372c0ad0286c4d84e6a96be625976e20a703
Jonhtan Mota <jmtrs@rede.ulbra.br>
2020-02-17 09:16:36 -0300

    Adicionado funções de exclusão de task, conclusão de task, renderização automatica de tasks e implementado adicionar tarefa.

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 class LStorage{
-    constructor() {
-        //testes
-        window.localStorage.setItem("teste", "aaa");
-        window.localStorage.setItem("teste2", "aaa2");
-    }
-
     //salvar no localStorage.
     salvar(data, titulo, status){
         window.localStorage.setItem(titulo, [data, titulo, status]);
@@ -15,6 +9,14 @@ class LStorage{
     excluir(id){
         window.localStorage.removeItem(id);
         console.log(`Task de id: "${id}" removida.`);
+    }
+
+    concluir(id) {
+        let task = JSON.parse(window.localStorage.getItem(id));
+        task["status"] = true;
+        const task_converted = JSON.stringify(task);
+        window.localStorage.setItem(id, task_converted);
+        console.log(`Task de id: "${id}" concluída."`);
     }
 
     //retorna a tarefa salva localmente.
@@ -55,24 +57,41 @@ class Tarefa {
 //variavel global da estrutura de tarefas
 let tasks = document.querySelectorAll('tbody')[0];
 //Adiciona no DOM as tasks
-function renderTask(data, titulo, status) {
+function renderTask(id, data, titulo, status) {
     //renderiza não completada
     if(status=="false" || status==false) {
-        tasks.insertAdjacentHTML("afterBegin", `<tr id='itensTabela'><th>${data}</th><td>${titulo}</td><td class='text-right'><button id="btnConcluir" type="button" class="btn btn-success">Conluir</button>
-        <button id="btnExcluir" type="button" class="btn btn-danger" onClick="removeTask(this)">Excluir</button></td></td></tr>`);
+        tasks.insertAdjacentHTML("afterBegin", `<tr id='itensTabela'><th>${data}</th><td>${titulo}</td><td class='text-right'><button id="btnConcluir" type="button" onClick="concluiTask(this)" name="${id}" class="btn btn-success">Conluir</button>
+        <button id="btnExcluir" type="button" class="btn btn-danger" onClick="removeTask(this)" name="${id}">Excluir</button></td></td></tr>`);
     } else { //renderiza os completados tracejado e sem o botão concluir.
-        tasks.insertAdjacentHTML("afterBegin", `<tr id='itensTabela'><th><strike>${data}</strike></th><td><strike>${titulo}</strike></td><td class='text-right'> <button id="btnExcluir" type="button" class="btn btn-danger" onClick="removeTask(this)">Excluir</button></td></td></tr>`);
+        tasks.insertAdjacentHTML("afterBegin", `<tr id='itensTabela'><th><strike>${data}</strike></th><td><strike>${titulo}</strike></td><td class='text-right'></td></td></tr>`);
     }
 }
 
 //Função para remover do DOM e excluir do localStorage
 function removeTask(ele, id=undefined) { //Exlui a task do DOM
-    ele.parentElement.parentElement.remove();
-    g_tasks.excluir(id);
+    if(id==undefined) {
+        g_tasks.excluir(ele['name']);
+        ele.parentElement.parentElement.remove();
+    } else {
+        ele.parentElement.parentElement.remove();
+    }
+}
+
+function concluiTask(ele, id=undefined) {
+    let id_salva = ele['name'];
+    g_tasks.concluir(ele['name']);
+    let tarefa = JSON.parse(localStorage.getItem(ele['name']));
+    removeTask(ele, false);
+    renderTask(id_salva, tarefa['date'].split("em"), tarefa['title'], tarefa['status']);
+}
+
+//retorna um número aleatorio entre 0-1024
+function random() {
+    return Math.floor(Math.random()*1024);
 }
 
 
-function lerTarefa1(){
+function lerTarefa(){
     let tarefa = document.getElementById("tituloTarefa").value
     console.log(tarefa);
     a =tarefa.split("em")
@@ -84,15 +103,25 @@ function lerTarefa1(){
     const userString = JSON.stringify(t);
 
     console.log(userString)
+    
+    let tarefa_id = random();
 
-
-    localStorage.setItem('test',userString);
-    r = localStorage.getItem('test');
+    localStorage.setItem(tarefa_id,userString);
     a = JSON.parse(userString)
     console.log("R: ", a.date)
     console.log("R: ", a.title)
     console.log("R: ", a.status)
 
+    let tarefa_atual = JSON.parse(localStorage.getItem(tarefa_id));
+    renderTask(tarefa_id, tarefa_atual["date"], tarefa_atual["title"], tarefa_atual["status"]);
+
     document.getElementById("tituloTarefa").value = ""
+}
+
+//renderiza todas as tasks salvas localmente
+for(task in g_tasks.getKeys()) {
+    console.log(localStorage.getItem(g_tasks.getKeys()[task]));
+    let tarefa = JSON.parse(localStorage.getItem(g_tasks.getKeys()[task]));
+    renderTask(g_tasks.getKeys()[task], tarefa["date"], tarefa["title"], tarefa["status"]);
 }
 
