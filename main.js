72b9c0eaaa97d3432f48356d8fe67764b4ebd515
Joao Paulo <joaopaulloojpos15@gmail.com>
2020-02-20 16:48:33 -0300

    Adicionado comentários no cdigo

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-class LStorage{
+class LStorage {
     //salvar no localStorage.
-    salvar(data, titulo, status){
+    salvar(data, titulo, status) {
         window.localStorage.setItem(titulo, [data, titulo, status]);
         console.log("task salva localmente.");
     }
 
     //excluir do localStorage.
-    excluir(id){
+    excluir(id) {
         window.localStorage.removeItem(id);
         console.log(`Task de id: "${id}" removida.`);
     }
 
+    //altera o status da tarefa para true
     concluir(id) {
         let task = JSON.parse(window.localStorage.getItem(id));
         task["status"] = true;
@@ -20,14 +21,14 @@ class LStorage{
     }
 
     //retorna a tarefa salva localmente.
-    getTask(chave){
+    getTask(chave) {
         return window.localStorage.getItem(chave);
     }
 
     //retorna uma array com todas as keys armazenadas para serem acessadas.
     getKeys() {
         this.keys = [];
-        for(let b in Object.entries(localStorage)){
+        for (let b in Object.entries(localStorage)) {
             //pegando as chaves e salvando na lista.
             this.keys.push(window.localStorage.key(b));
         };
@@ -38,6 +39,7 @@ class LStorage{
 g_tasks = new LStorage();
 console.log("keys salvas no storage: " + g_tasks.getKeys());
 
+//classe Tarefa
 class Tarefa {
     constructor(data, titulo, status) {
         this.date = data;
@@ -45,21 +47,15 @@ class Tarefa {
         this.status = status;
 
     }
-
-    
-    adicionarTarefa(data, titulo) {
-
-        t1 = new Tarefa('12/02/2020', 'lavar o carro', 'false')
-        console.log(t1)
-    }
 }
 
 //variavel global da estrutura de tarefas
 let tasks = document.querySelectorAll('tbody')[0];
+
 //Adiciona no DOM as tasks
 function renderTask(id, data, titulo, status) {
     //renderiza não completada
-    if(status=="false" || status==false) {
+    if (status == "false" || status == false) {
         tasks.insertAdjacentHTML("afterBegin", `<tr id='itensTabela'><th>${data}</th><td>${titulo}</td><td class='text-right'><button id="btnConcluir" type="button" onClick="concluiTask(this)" name="${id}" class="btn btn-success">Conluir</button>
         <button id="btnExcluir" type="button" class="btn btn-danger" onClick="removeTask(this)" name="${id}">Excluir</button></td></td></tr>`);
     } else { //renderiza os completados tracejado e sem o botão concluir.
@@ -68,8 +64,8 @@ function renderTask(id, data, titulo, status) {
 }
 
 //Função para remover do DOM e excluir do localStorage
-function removeTask(ele, id=undefined) { //Exlui a task do DOM
-    if(id==undefined) {
+function removeTask(ele, id = undefined) { //Exlui a task do DOM
+    if (id == undefined) {
         g_tasks.excluir(ele['name']);
         ele.parentElement.parentElement.remove();
     } else {
@@ -77,7 +73,8 @@ function removeTask(ele, id=undefined) { //Exlui a task do DOM
     }
 }
 
-function concluiTask(ele, id=undefined) {
+//conclui a tarefa e chama as funções removeTask e renderTask
+function concluiTask(ele, id = undefined) {
     let id_salva = ele['name'];
     g_tasks.concluir(ele['name']);
     let tarefa = JSON.parse(localStorage.getItem(ele['name']));
@@ -87,26 +84,24 @@ function concluiTask(ele, id=undefined) {
 
 //retorna um número aleatorio entre 0-1024
 function random() {
-    return Math.floor(Math.random()*1024);
+    return Math.floor(Math.random() * 1024);
 }
 
-function formatDate(date){
-    return date.replace(/\/[0-9]*$/,"");
+//formata a data
+function formatDate(date) {
+    return date.replace(/\/[0-9]*$/, "");
 }
 
-
-function lerTarefa(){
+//ler a tarefa, popula um objeto de Tarefa, adiciona no LocalStorage, chama a função renderTask e limpa o campo input
+function lerTarefa() {
     let tarefa = document.getElementById("tituloTarefa").value
-    a =tarefa.split("em")
-    t = new Tarefa(formatDate(a[1]), a[0], "false")
+    listaTarefa = tarefa.split("em")
+    objetoTarefa = new Tarefa(formatDate(listaTarefa[1]), listaTarefa[0], "false")
 
-    const userString = JSON.stringify(t);
-
-    
+    const userString = JSON.stringify(objetoTarefa);
     let tarefa_id = random();
 
-    localStorage.setItem(tarefa_id,userString);
-    a = JSON.parse(userString)
+    localStorage.setItem(tarefa_id, userString);
 
     let tarefa_atual = JSON.parse(localStorage.getItem(tarefa_id));
     renderTask(tarefa_id, tarefa_atual["date"], tarefa_atual["title"], tarefa_atual["status"]);
@@ -115,8 +110,7 @@ function lerTarefa(){
 }
 
 //renderiza todas as tasks salvas localmente
-for(task in g_tasks.getKeys()) {
+for (task in g_tasks.getKeys()) {
     let tarefa = JSON.parse(localStorage.getItem(g_tasks.getKeys()[task]));
     renderTask(g_tasks.getKeys()[task], tarefa["date"], tarefa["title"], tarefa["status"]);
 }
-
